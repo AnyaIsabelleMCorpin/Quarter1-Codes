146290f6d720aeaa282850892fcf710543578b02
AnyaIsabelleMCorpin <anyaisabelle.corpin@smc.pshs.edu.ph>
2025-10-08 19:52:30 +0800

    Add files via upload

---
+++ CS2Q1SG4_Grade_Evaluation.py
@@ -0,0 +1,9 @@
+grade = int(input("Enter grade: "))
+if grade >= 90:
+    print("Grade = A")
+elif grade >= 80:
+    print("Grade = B")
+elif grade >= 70:
+    print("Grade = C")
+else:
+    print("Grade = D")
